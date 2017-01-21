55ebc8bd220948cbe1d2d129c0eb109aa2232531
kathnixcarnell <kathsnc+github@gmail.com>
2017-01-21 11:06:30 +1100

    Split up the spacebar functions a little more

--- MelbGGJ2017/Assets/Scripts/GoddessController.cs
+++ MelbGGJ2017/Assets/Scripts/GoddessController.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class GoddessController : MonoBehaviour
 {
 
+    private float spacebarTime;
+    private bool spaceBarDown = false;
+
     public void Enable()
     {
-
+        Debug.Log("GoddessController Enable");
     }
 
     // Update is called once per frame
     void Update()
     {
+        TimeSpaceBar();
+    }
 
+    private void TimeSpaceBar()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+            spacebarTime = 0f;
+            Debug.Log("STARTED SPACING AT " + spacebarTime);
+            spaceBarDown = true;
+        }
+        if (Input.GetKey("space"))
+        {
+
+            spacebarTime += Time.deltaTime;
+            Debug.Log("Spacebar pressed, current length: " + spacebarTime);
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            Debug.Log("RELEASED THE SPACEBAR AFTER " + spacebarTime);
+            spaceBarDown = false;
+        }
     }
 }
